be4b0b24485596f4808cc275d52b62d6b31c8739
Hector Ricardo <hectorricardomendez@gmail.com>
2021-09-30 01:01:03 -0500

    Removing dead code

--- Source/Estimation/Estimator.swift
+++ Source/Estimation/Estimator.swift
@@ -10,22 +10,4 @@ extension Estimator {
   func estimateFrequency(sampleRate: Float, location: Int, bufferCount: Int) -> Float {
     return Float(location) * sampleRate / (Float(bufferCount) * 2)
   }
-
-  func maxBufferIndex(from buffer: [Float]) throws -> Int {
-    guard buffer.count > 0 else {
-      throw EstimationError.emptyBuffer
-    }
-
-    guard let index = buffer.maxIndex else {
-      throw EstimationError.unknownMaxIndex
-    }
-
-    return index
-  }
-
-  func sanitize(location: Int, reserveLocation: Int, elements: [Float]) -> Int {
-    return location >= 0 && location < elements.count
-      ? location
-      : reserveLocation
-  }
 }
